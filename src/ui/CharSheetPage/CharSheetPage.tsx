9cb5d8c7b79852244d67b5c95e9c6335ff072326
NtsDK <ghoul71@yandex.ru>
2022-06-16 02:49:17 +0200

    Initial profile structure

--- src/ui/CharSheetPage/CharSheetPage.tsx
+++ src/ui/CharSheetPage/CharSheetPage.tsx
@@ -1,15 +1,71 @@
 import React from 'react';
 import './CharSheetPage.css';
 
+import { useTranslation } from 'react-i18next';
+
 interface CharSheetPageProps {
 }
 
 export function CharSheetPage(props: CharSheetPageProps) {
-  // const { t } = props;
+  const { t } = useTranslation();
 
   return (
     <div className="CharSheetPage">
       CharSheetPage content
+
+      {/* profile */}
+      <div className="custom-panel">
+        <div className="columns profile-container">
+          <div>
+            <div>
+              <span>{t('charsheet.name')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.player')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.chronicle')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.age')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.sex')}</span>
+              <input/>
+            </div>
+          </div>
+          <div>
+            <div>
+              <span>{t('charsheet.nature')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.demeanor')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.concept')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.clan')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.generation')}</span>
+              <input/>
+            </div>
+            <div>
+              <span>{t('charsheet.sire')}</span>
+              <input/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
