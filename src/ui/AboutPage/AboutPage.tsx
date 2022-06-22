81276ad67df5fb5058653e6ea105f798899859b2
NtsDK <ghoul71@yandex.ru>
2022-06-22 02:43:34 +0200

    Add basic about information.

--- src/ui/AboutPage/AboutPage.tsx
+++ src/ui/AboutPage/AboutPage.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import './AboutPage.css';
 
+import Card from 'react-bootstrap/Card';
+import { useTranslation } from 'react-i18next';
+
 interface AboutPageProps {
 }
 
 export function AboutPage(props: AboutPageProps) {
-  // const { t } = props;
+  const { t } = useTranslation();
 
   return (
     <div className="AboutPage">
-      AboutPage content
+      <Card>
+        <Card.Body>
+          <div>{t('about.about-authors')}</div>
+          <br/>
+          <span>{t('about.site-mention')} </span>
+          <a href="http://trechkalov.com">trechkalov.com</a>
+          <span>{t('about.site-description')}</span>
+          <br/>
+          <br/>
+          <span>{t('about.program-is-free-in-rep')}</span>
+          <a href="https://github.com/NtsDK/vtmcl">{t('about.by-link')}</a>
+          <br/>
+          <br/>
+          <div>{t('about.icons-authors')}</div>
+          <br/>
+          <div>{t('about.versions')}</div>
+          <ul>
+            <li>{t('about.var010')}</li>
+          </ul>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
