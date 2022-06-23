b8fbabe4ad47ad8a607caab655480b76d48c76b4
NtsDK <ghoul71@yandex.ru>
2022-06-23 03:10:56 +0200

    Updating about page

--- src/ui/AboutPage/AboutPage.tsx
+++ src/ui/AboutPage/AboutPage.tsx
@@ -16,8 +16,7 @@ export function AboutPage(props: AboutPageProps) {
         <Card.Body>
           <div>{t('about.about-authors')}</div>
           <br/>
-          <span>{t('about.site-mention')} </span>
-          <a href="http://trechkalov.com">trechkalov.com</a>
+          <a href="http://trechkalov.com">{t('about.site-mention')} trechkalov.com</a>
           <span>{t('about.site-description')}</span>
           <br/>
           <br/>
@@ -31,6 +30,65 @@ export function AboutPage(props: AboutPageProps) {
           <ul>
             <li>{t('about.var010')}</li>
           </ul>
+          <br/>
+          <br/>
+          <br/>
+
+
+          <div>
+            <div className="tw-mb-8">
+              <p>Лист персонажа VtM V20 от NtsDK</p>
+              <p>Версия 0.2.0 от 23.06.2022</p>
+              <p>Проект с открытым исходным кодом под лицензией Apache 2.0</p>
+              <p>В проекте используются иконки с сайта <a href="www.flaticon.com">www.flaticon.com</a> от авторов Freepik, Picol, Nice and Serious.</p>
+              <p>В проекте используются флаги с сайта <a href="flagicons.lipis.dev">flagicons.lipis.dev</a></p>
+            </div>
+
+            <div className="tw-mb-8">
+              <h2 className="tw-text-lg tw-mb-4">Ссылки</h2>
+              <ul>
+                <li>
+                  <a href="http://trechkalov.com">Сайт разработчика: trechkalov.com</a>
+                </li>
+                <li>
+                  <a href="https://vk.com/ntsdk_wod_software">Группа проекта вконтакте</a>
+                </li>
+                <li>
+                  <a href="https://t.me/ntsdk_wod_software">Канал проекта в телеграмме</a>
+                </li>
+                <li>
+                  <a href="https://t.me/ntsdk_wod_software_chat">Чат проекта в телеграмме</a>
+                </li>
+                <li>
+                  <a href="https://github.com/NtsDK/vtmcl">Исходный код проекта</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="tw-mb-8">
+              <h2 className="tw-text-lg tw-mb-4">История версий</h2>
+              <ul>
+                <li>
+                  Версия 0.2.0 (23 Jun 2022) - проект переписан с нуля с сохранением обратной совместимости и оригинального дизайна.
+                </li>
+                <li>
+                  Версия 0.1.0 (7 Nov 2017) - проект 'Лист персонажа Vampire: the Masquerade' интегрирован в проект НИМС. Реализован базовый лист персонажа для VtM 4ой редакции с поддержкой русского и английского языков.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="tw-text-lg">Участники проекта</h2>
+            <ul>
+              <li>
+                Тимофей NtsDK Речкалов - разработка
+              </li>
+              <li>
+                Павел Синеглазов - дизайн
+              </li>
+            </ul>
+          </div>
         </Card.Body>
       </Card>
     </div>
