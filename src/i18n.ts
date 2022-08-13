e6b86bbaa3f1077ce96eb56d1ac65c04e433c421
NtsDK <ghoul71@yandex.ru>
2022-08-13 23:04:46 +0200

    Fix lang tag

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,21 @@
 import i18n from 'i18next';
 import { DefaultNamespace, initReactI18next, TFuncKey, Resources } from 'react-i18next';
 import { resources, defaultLang } from './i18nResources';
+// see https://www.npmjs.com/package/i18next-browser-languagedetector
+// for details
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n.on('languageChanged', (lng) => {
+  localStorage.setItem('i18nextLng', lng);
+});
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
-    lng: 'ru',
+    // lng: lang,
+    fallbackLng: 'en',
 
     // keySeparator: false, // we do not use keys in form messages.welcome
 
