616adbf2137b69f4a1b3e53ca0b3534ff40aaa74
NtsDK <ghoul71@yandex.ru>
2022-09-18 01:53:25 +0200

    Extract RatingPoolSection

--- src/ui/CharSheetPage/commonBlocks/WillSection/WillSection.tsx
+++ src/ui/CharSheetPage/commonBlocks/WillSection/WillSection.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import classnames from "classnames";
 
-import { RangeInput2 } from '../../primitives/RangeInput2';
 import { useStateNHealth } from '../../../../services/storageAdapter';
+import { RatingPoolSection } from '../../primitives/RatingPoolSection';
 
 import './WillSection.css';
 
@@ -17,28 +16,16 @@ export function WillSection(props: WillSectionProps) {
   const { className } = props;
 
   return (
-    <div className={classnames("WillSection", className)}>
-      <fieldset aria-label={t('charsheet.status.willpower-rating')}>
-        <RangeInput2
-          max={10}
-          name="willpowerRating"
-          value={state.willpowerRating}
-          onClick={(value: number) => setState('willpowerRating', value)}
-          className="tw-h-6"
-          multiplier={1.3}
-        />
-      </fieldset>
-      <fieldset aria-label={t('charsheet.status.willpower-pool')}>
-        <RangeInput2
-          max={10}
-          name="willpowerPool"
-          value={state.willpowerPool}
-          onClick={(value: number) => setState('willpowerPool', value)}
-          className="tw-h-6"
-          variant='square'
-          multiplier={1.3}
-        />
-      </fieldset>
-    </div>
+    <RatingPoolSection
+      name='willpower'
+      pool={state.willpowerPool}
+      poolAriaLabel={t('charsheet.status.willpower-pool')}
+      rating={state.willpowerRating}
+      ratingAriaLabel={t('charsheet.status.willpower-rating')}
+      setPool={(value) => setState('willpowerPool', value)}
+      setRating={(value) => setState('willpowerRating', value)}
+      className={className}
+    />
   );
 }
+
