f3fe84ddfd7b53a640968211c58144c9c1dae20b
NtsDK <ghoul71@yandex.ru>
2022-09-22 02:35:23 +0200

    Work on c20 merits and flaws

---
+++ src/i18nResources/dropdownContent/changeling/meritsAndFlaws.ts
@@ -0,0 +1,116 @@
+import * as R from 'ramda';
+
+import {
+  meritsAndFlawsGroupsSource,
+  mentalFlawsSource,
+  mentalMeritsSource,
+  physicalFlawsSource,
+  physicalMeritsSource,
+  socialFlawsSource,
+  socialMeritsSource,
+  supernaturalFlawsSource,
+  supernaturalMeritsSource,
+} from "./meritsAndFlawsSource";
+
+import {
+  generateEnRuEntities,
+  generateSequence,
+  makeTranslateFunction,
+  generateIdEnRuEntities,
+  sortStrArr,
+  IdEnRuEntity
+} from '../utils';
+
+type MeritsAndFlawsGroups =
+  | 'physical-merits'
+  | 'physical-flaws'
+  | 'mental-merits'
+  | 'mental-flaws'
+  | 'social-merits'
+  | 'social-flaws'
+  | 'supernatural-merits'
+  | 'supernatural-flaws'
+;
+
+const mentalFlawsArr = [...generateEnRuEntities(generateSequence(2, mentalFlawsSource))];
+const mentalMeritsArr = [...generateEnRuEntities(generateSequence(2, mentalMeritsSource))];
+const physicalFlawsArr = [...generateEnRuEntities(generateSequence(2, physicalFlawsSource))];
+const physicalMeritsArr = [...generateEnRuEntities(generateSequence(2, physicalMeritsSource))];
+const socialFlawsArr = [...generateEnRuEntities(generateSequence(2, socialFlawsSource))];
+const socialMeritsArr = [...generateEnRuEntities(generateSequence(2, socialMeritsSource))];
+const supernaturalFlawsArr = [...generateEnRuEntities(generateSequence(2, supernaturalFlawsSource))];
+const supernaturalMeritsArr = [...generateEnRuEntities(generateSequence(2, supernaturalMeritsSource))];
+
+const allMeritsAndFlaws = [
+  ...mentalFlawsArr,
+  ...mentalMeritsArr,
+  ...physicalFlawsArr,
+  ...physicalMeritsArr,
+  ...socialFlawsArr,
+  ...socialMeritsArr,
+  ...supernaturalFlawsArr,
+  ...supernaturalMeritsArr,
+];
+
+export const translateMeritsAndFlaws = makeTranslateFunction(allMeritsAndFlaws);
+
+
+const meritsAndFlawsGroupsArr = [...generateIdEnRuEntities(generateSequence(3, meritsAndFlawsGroupsSource))];
+
+const groupNames = R.indexBy(R.prop('id'), meritsAndFlawsGroupsArr) as Record<MeritsAndFlawsGroups, IdEnRuEntity>;
+
+export const merits_en = [{
+  groupName: groupNames['physical-merits'].en,
+  arr: sortStrArr(R.pluck('en', physicalMeritsArr))
+}, {
+  groupName: groupNames['mental-merits'].en,
+  arr: sortStrArr(R.pluck('en', mentalMeritsArr))
+}, {
+  groupName: groupNames['social-merits'].en,
+  arr: sortStrArr(R.pluck('en', socialMeritsArr))
+}, {
+  groupName: groupNames['supernatural-merits'].en,
+  arr: sortStrArr(R.pluck('en', supernaturalMeritsArr))
+}];
+
+export const merits_ru = [{
+  groupName: groupNames['physical-merits'].ru,
+  arr: sortStrArr(R.pluck('ru', physicalMeritsArr))
+}, {
+  groupName: groupNames['mental-merits'].ru,
+  arr: sortStrArr(R.pluck('ru', mentalMeritsArr))
+}, {
+  groupName: groupNames['social-merits'].ru,
+  arr: sortStrArr(R.pluck('ru', socialMeritsArr))
+}, {
+  groupName: groupNames['supernatural-merits'].ru,
+  arr: sortStrArr(R.pluck('ru', supernaturalMeritsArr))
+}];
+
+export const flaws_en = [{
+  groupName: groupNames['physical-flaws'].en,
+  arr: sortStrArr(R.pluck('en', physicalFlawsArr))
+}, {
+  groupName: groupNames['mental-flaws'].en,
+  arr: sortStrArr(R.pluck('en', mentalFlawsArr))
+}, {
+  groupName: groupNames['social-flaws'].en,
+  arr: sortStrArr(R.pluck('en', socialFlawsArr))
+}, {
+  groupName: groupNames['supernatural-flaws'].en,
+  arr: sortStrArr(R.pluck('en', supernaturalFlawsArr))
+}];
+
+export const flaws_ru = [{
+  groupName: groupNames['physical-flaws'].ru,
+  arr: sortStrArr(R.pluck('ru', physicalFlawsArr))
+}, {
+  groupName: groupNames['mental-flaws'].ru,
+  arr: sortStrArr(R.pluck('ru', mentalFlawsArr))
+}, {
+  groupName: groupNames['social-flaws'].ru,
+  arr: sortStrArr(R.pluck('ru', socialFlawsArr))
+}, {
+  groupName: groupNames['supernatural-flaws'].ru,
+  arr: sortStrArr(R.pluck('ru', supernaturalFlawsArr))
+}];
