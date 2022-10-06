40dde6eb1f7e7f613141c1bbd0962ab03cedc28f
NtsDK <ghoul71@yandex.ru>
2022-10-07 01:21:34 +0200

    Rituals and discipline paths lists

---
+++ src/i18nResources/dropdownContent/vampire/disciplinePathsSource.ts
@@ -0,0 +1,104 @@
+// Format:
+// <en>
+//   <ru>
+//     groupName
+
+export const disciplinePathsSource = `Awakening of the Steel
+  Пробуждение Стали
+    assamite_sorcery
+The Way of Earth
+  Уклад Земли
+    koldunic_sorcery
+The Way of Wind
+  Уклад Ветра
+    koldunic_sorcery
+The Way of Water
+  Уклад Воды
+    koldunic_sorcery
+The Way of Fire
+  Уклад Огня
+    koldunic_sorcery
+The Sepulchre Path
+  Путь Склепа
+    necromancy
+The Ash Path
+  Путь Пепла
+    necromancy
+The Bone Path
+  Путь Костей
+    necromancy
+The Cenotaph Path
+  Путь Кенотафа
+    necromancy
+The Corpse in the Monster
+  Путь Мертвеца
+    necromancy
+The Grave’s Decay
+  Путь Тлена
+    necromancy
+Path of the Four Humors
+  Путь Четырёх Гуморов
+    necromancy
+Vitreous Path
+  Путь Энтропии
+    necromancy
+The Path of Blood
+  Путь Крови
+    thaumaturgy
+Elemental Mastery
+  Власть над Стихиями
+    thaumaturgy
+The Green Path
+  Путь Флоры
+    thaumaturgy
+Hands of Destruction
+  Длань Разрушения
+    thaumaturgy
+The Lure of Flames
+  Игра с Огнём
+    thaumaturgy
+Neptune’s Might
+  Могущество Нептуна
+    thaumaturgy
+Movement of the Mind
+  Мановение Мысли
+    thaumaturgy
+The Path of Conjuring
+  Путь Воплощения
+    thaumaturgy
+The Path of Corruption
+  Путь Совращения
+    thaumaturgy
+The Path of Mars
+  Путь Марса
+    thaumaturgy
+The Path of Technomancy
+  Путь Техномантии
+    thaumaturgy
+The Path of the Father’s Vengeance
+  Путь Отчего Воздаяния
+    thaumaturgy
+Thaumaturgical Countermagic
+  Тауматургическая Защита
+    thaumaturgy
+Weather Control
+  Управление Погодой
+    thaumaturgy`;
+
+// Format:
+// <id>
+//   <en>
+//     <ru>
+
+export const disciplinePathGroupsSource = `assamite_sorcery
+  Assamite Sorcery
+    Чародейство Ассамитов
+koldunic_sorcery
+  Koldunic Sorcery
+    Колдовство
+necromancy
+  Necromancy
+    Некромантия
+thaumaturgy
+  Thaumaturgy
+    Тауматургия`;
---
+++ src/i18nResources/dropdownContent/vampire/ritualsSource.ts
@@ -0,0 +1,242 @@
+// Format:
+// <en>
+//   <ru>
+//     groupName
+
+export const ritualsSource = `Call of the Hungry Dead
+  Зов ненасытной смерти
+    necromantic-1
+Minestra di Morte
+  Минестра ди морте
+    necromantic-1
+Insight
+  Озарение
+    necromantic-1
+Eldritch Beacon
+  Потусторонний маяк
+    necromantic-1
+Ritual of the Smoking Mirror
+  Ритуал Тескатлипоки
+    necromantic-1
+Knowing Stone
+  Указующий камень
+    necromantic-1
+Eyes of the Grave
+  Взгляд в могилу
+    necromantic-2
+Two Centimes
+  Два сантима
+    necromantic-2
+Puppet
+  Марионетка
+    necromantic-2
+Occhio d’Uomo Morto
+  Оккио д’уомо морто
+    necromantic-2
+The Ritual of Pochtli
+  Ритуал Уицилопочтли
+    necromantic-2
+The Hand of Glory
+  Рука славы
+    necromantic-2
+Nightmare Drums
+  Барабаны ночных кошмаров
+    necromantic-3
+Din of the Damned
+  Гам проклятых
+    necromantic-3
+Divine Sign
+  Знак свыше
+    necromantic-3
+Ritual of The Unearthed Fetter
+  Поиск призрачного якоря
+    necromantic-3
+Blood Dance
+  Танец крови
+    necromantic-3
+Tempesta Scudo
+  Темпеста скудо
+    necromantic-3
+Bastone Diabolico
+  Бастоне диаболико
+    necromantic-4
+Peek Past the Shroud
+  Взгляд за Завесу
+    necromantic-4
+Baleful Doll
+  Зловещая кукла
+    necromantic-4
+Cadaver’s Touch
+  Касание мертвеца
+    necromantic-4
+Ritual of Xipe Totec
+  Ритуал Шипе-Тотека
+    necromantic-4
+Grasp the Ghostly
+  Призрачная добыча
+    necromantic-5
+Dead Man’s Hand
+  Рука мертвеца
+    necromantic-5
+Chill of Oblivion
+  Холод забвения
+    necromantic-5
+Esilio
+  Эзилио
+    necromantic-5
+Wake with Evening’s Freshness
+  Вечерняя бодрость
+    thaumaturgical-1
+Domino of Life
+  Видимость жизни
+    thaumaturgical-1
+Incantation of the Shepherd
+  Заклинание пастыря
+    thaumaturgical-1
+Defense of the Sacred Haven
+  Защита неприкосновенного убежища
+    thaumaturgical-1
+Widow’s Spite
+  Злая вдова
+    thaumaturgical-1
+Devil’s Touch
+  Метка Дьявола
+    thaumaturgical-1
+Deflection of Wooden Doom
+  Отторжение губительного древа
+    thaumaturgical-1
+Illuminate the Trail of Prey
+  Свечение следопыта
+    thaumaturgical-1
+Communicate with Kindred Sire
+  Связь с сиром
+    thaumaturgical-1
+Bind the Accusing Tongue
+  Смежение злословных уст
+    thaumaturgical-1
+Engaging the Vessel of Transference
+  Создание переливающего сосуда
+    thaumaturgical-1
+Blood Rush
+  Ток крови
+    thaumaturgical-1
+Purity of Flesh
+  Чистота плоти
+    thaumaturgical-1
+Ward Versus Ghouls
+  Защитная печать против гулей
+    thaumaturgical-2
+Warding Circle versus Ghouls
+  Защитный круг против гулей
+    thaumaturgical-2
+Donning the Mask of Shadows
+  Облачение в маску теней
+    thaumaturgical-2
+Eyes of the Night Hawk
+  Око ночного ястреба
+    thaumaturgical-2
+Machine Blitz
+  Отключение машины
+    thaumaturgical-2
+Blood Walk
+  Постижение Крови
+    thaumaturgical-2
+Burning Blade
+  Пылающий клинок
+    thaumaturgical-2
+Principal Focus of Vitae Infusion
+  Хранилище пролитой витэ
+    thaumaturgical-2
+Recure of the Homeland
+  Целебная сила родной земли
+    thaumaturgical-2
+Incorporeal Passage
+  Бесплотный путь
+    thaumaturgical-3
+Sanguine Assistant
+  Гомункул-помощник
+    thaumaturgical-3
+Shaft of Belated Quiescence
+  Жезл неизбежного покоя
+    thaumaturgical-3
+Ward versus Lupines
+  Защитная печать против Люпенов
+    thaumaturgical-3
+Mirror of Second Sight
+  Зеркало второго зрения
+    thaumaturgical-3
+Flesh of Fiery Touch
+  Огненная плоть
+    thaumaturgical-3
+Pavis of Foul Presence
+  Отражённое Величие
+    thaumaturgical-3
+Clinging of the Insect
+  Паучьи лапы
+    thaumaturgical-3
+Splinter Servant
+  Деревянный слуга
+    thaumaturgical-4
+Ward versus Kindred
+  Защитная печать против Сородичей
+    thaumaturgical-4
+Heart of Stone
+  Каменное сердце
+    thaumaturgical-4
+Bone of Lies
+  Кость лжи
+    thaumaturgical-4
+Firewalker
+  Пожиратель пламени
+    thaumaturgical-4
+Paper Flesh
+  Бумажная плоть
+    thaumaturgical-5
+Escape to a True Friend
+  Врата истинной дружбы
+    thaumaturgical-5
+Enchant Talisman
+  Зачарование талисмана
+    thaumaturgical-5
+Ward versus Spirits
+  Защитная печать против духов
+    thaumaturgical-5
+Blood Contract
+  Кровавый договор
+    thaumaturgical-5`;
+
+// Format:
+// <id>
+//   <en>
+//     <ru>
+
+export const ritualGroupsSource = `necromantic-1
+  Necromantic Level One Rituals
+    Некромантические ритуалы первого уровня
+necromantic-2
+  Necromantic Level Two Rituals
+    Некромантические ритуалы второго уровня
+necromantic-3
+  Necromantic Level Three Rituals
+    Некромантические ритуалы третьего уровня
+necromantic-4
+  Necromantic Level Four Rituals
+    Некромантические ритуалы четвёртого уровня
+necromantic-5
+  Necromantic Level Five Rituals
+    Некромантические ритуалы пятого уровня
+thaumaturgical-1
+  Thaumaturgical Level One Rituals
+    Тауматургические ритуалы первого уровня
+thaumaturgical-2
+  Thaumaturgical Level Two Rituals
+    Тауматургические ритуалы второго уровня
+thaumaturgical-3
+  Thaumaturgical Level Three Rituals
+    Тауматургические ритуалы третьего уровня
+thaumaturgical-4
+  Thaumaturgical Level Four Rituals
+    Тауматургические ритуалы четвёртого уровня
+thaumaturgical-5
+  Thaumaturgical Level Five Rituals
+    Тауматургические ритуалы пятого уровня`;
