9c33d573466bfcba460e62b08ae21e2a2bfc984b
NtsDK <ghoul71@yandex.ru>
2023-01-18 04:21:08 +0500

    Extend changeling charsheet

--- src/ui/CharSheetPage/ChangelingCharSheet/ChangelingCharSheet.tsx
+++ src/ui/CharSheetPage/ChangelingCharSheet/ChangelingCharSheet.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { AdvantagesSection } from '../changelingBlocks/AdvantagesSection';
 import { StatusSection } from '../changelingBlocks/StatusSection';
+import { AlliesAndContactsSection } from '../commonBlocks/AlliesAndContactsSection';
+import { AppearanceDescriptionSection } from '../commonBlocks/AppearanceDescriptionSection';
+import { CharacterImageSection } from '../commonBlocks/CharacterImageSection';
 import { CharHistorySection } from '../commonBlocks/CharHistorySection';
 import { FlawsSection } from '../commonBlocks/FlawsSection';
 import { GoalsSection } from '../commonBlocks/GoalsSection';
 import { MeritsSection } from '../commonBlocks/MeritsSection';
 import { NotesSection } from '../commonBlocks/NotesSection';
+import { OtherTraitsSection } from '../commonBlocks/OtherTraitsSection';
+import { PossessionsSection } from '../commonBlocks/PossessionsSection';
 import { CommonPageStart } from '../CommonPageStart';
 import { CharSheetBody } from '../primitives/CharSheetBody';
 import { SectionHeader } from '../primitives/SectionHeader';
@@ -50,20 +55,61 @@ export function ChangelingCharSheet(props: ChangelingCharSheetProps) {
           </div>
         </div>
 
-        <SectionHeader className="tw-mb-3">
-          {t('charsheet.charHistory')}
-        </SectionHeader>
-        <CharHistorySection className="tw-mb-6"/>
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t('charsheet.advantages.otherTraits')}
+            </SectionHeader>
+            <OtherTraitsSection/>
+          </div>
 
+          <div className="tw-flex-1" style={{flexGrow: 2}}>
+            <SectionHeader className="tw-mb-3">
+              {t('charsheet.charHistory')}
+            </SectionHeader>
+            <CharHistorySection className="tw-mb-6"/>
+
+            <SectionHeader className="tw-mb-3">
+              {t('charsheet.goals')}
+            </SectionHeader>
+            <GoalsSection className="tw-mb-6"/>
+          </div>
+        </div>
+
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t('charsheet.alliesAndContacts')}
+            </SectionHeader>
+            <AlliesAndContactsSection/>
+          </div>
+
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t('charsheet.possessions')}
+            </SectionHeader>
+            <PossessionsSection/>
+          </div>
+        </div>
+      </CharSheetBody>
+      <CharSheetBody>
         <SectionHeader className="tw-mb-3">
-          {t('charsheet.goals')}
+          {t('charsheet.appearanceDescription')}
         </SectionHeader>
-        <GoalsSection className="tw-mb-6"/>
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <CharacterImageSection/>
+          </div>
+
+          <div className="tw-flex-1" style={{flexGrow: 2}}>
+            <AppearanceDescriptionSection/>
+          </div>
+        </div>
 
         <SectionHeader className="tw-mb-3">
           {t('charsheet.notes')}
         </SectionHeader>
-        <NotesSection />
+        <NotesSection/>
       </CharSheetBody>
     </>
   );
