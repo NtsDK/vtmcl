9bf8500506bc7df2a934d4226782355bac5f6ca6
NtsDK <ghoul71@yandex.ru>
2023-04-11 01:08:29 +0500

    Add checkAttributesFilled tests

--- src/tests/commonCharacterChecks.spec.ts
+++ src/tests/commonCharacterChecks.spec.ts
@@ -4,14 +4,93 @@ import {
   EXPECTED_ABILITY_DOTS,
   checkAbilitiesDotLimit,
   ABILITY_LIMIT,
+  checkAttributesFilled,
+  EXPECTED_ATTRIBUTE_DOTS,
 } from "../domainServices";
+import { attributesConfig } from "../i18nResources/presetSettings/attributesConfig";
 import { vampireAbilitiesConfig } from "../i18nResources/presetSettings/vampireAbilitiesConfig";
 import {
   initialAbilities,
   initialAbilitiesExtension,
+  initialAttributes,
 } from "../services/initialValues";
 
 describe("Common character checks", () => {
+  describe("checkAttributesFilled", () => {
+    it("Attributes zero - invalid", () => {
+      const attributes = R.clone(initialAttributes);
+      expect(
+        checkAttributesFilled(
+          attributes,
+          attributesConfig,
+          EXPECTED_ATTRIBUTE_DOTS
+        )
+      ).toStrictEqual({
+        arr: [0, 0, 0],
+        checked: false,
+      });
+    });
+    it("Attributes small non zero - invalid", () => {
+      const attributes = R.clone(initialAttributes);
+      attributes.strength = 3;
+      attributes.charisma = 3;
+      attributes.perception = 3;
+
+      expect(
+        checkAttributesFilled(
+          attributes,
+          attributesConfig,
+          EXPECTED_ATTRIBUTE_DOTS
+        )
+      ).toStrictEqual({
+        arr: [2, 2, 2],
+        checked: false,
+      });
+    });
+    it("Attributes filled right - valid", () => {
+      const attributes = R.clone(initialAttributes);
+      attributes.strength = 4;
+      attributes.charisma = 3;
+      attributes.manipulation = 4;
+      attributes.perception = 3;
+      attributes.intelligence = 3;
+      attributes.wits = 4;
+
+      expect(
+        checkAttributesFilled(
+          attributes,
+          attributesConfig,
+          EXPECTED_ATTRIBUTE_DOTS
+        )
+      ).toStrictEqual({
+        arr: [7, 5, 3],
+        checked: true,
+      });
+    });
+    it("Attributes filled to much - invalid", () => {
+      const attributes = R.clone(initialAttributes);
+      attributes.strength = 4;
+      attributes.dexterity = 4;
+      attributes.stamina = 4;
+      attributes.charisma = 3;
+      attributes.manipulation = 4;
+      attributes.perception = 3;
+      attributes.intelligence = 3;
+      attributes.wits = 4;
+
+      expect(
+        checkAttributesFilled(
+          attributes,
+          attributesConfig,
+          EXPECTED_ATTRIBUTE_DOTS
+        )
+      ).toStrictEqual({
+        arr: [9, 7, 5],
+        checked: false,
+      });
+    });
+  });
+
   describe("checkAbilitiesFilled", () => {
     it("Main abilities zero - valid", () => {
       const abilities = R.clone(initialAbilities);
@@ -108,74 +187,76 @@ describe("Common character checks", () => {
   // checkAbilitiesDotLimit,
   // ABILITY_LIMIT,
 
-  it("Default abilities and extra abilities are less than ABILITY_LIMIT - valid", () => {
-    const abilities = R.clone(initialAbilities);
-    const abilitiesExtension = R.clone(initialAbilitiesExtension);
-    expect(
-      checkAbilitiesDotLimit(
-        abilities,
-        vampireAbilitiesConfig,
-        abilitiesExtension,
-        ABILITY_LIMIT
-      )
-    ).toStrictEqual(true);
-  });
+  describe("checkAbilitiesDotLimit", () => {
+    it("Default abilities and extra abilities are less than ABILITY_LIMIT - valid", () => {
+      const abilities = R.clone(initialAbilities);
+      const abilitiesExtension = R.clone(initialAbilitiesExtension);
+      expect(
+        checkAbilitiesDotLimit(
+          abilities,
+          vampireAbilitiesConfig,
+          abilitiesExtension,
+          ABILITY_LIMIT
+        )
+      ).toStrictEqual(true);
+    });
 
-  it("Main abilities are less or equal to ABILITY_LIMIT - valid", () => {
-    const abilities = R.clone(initialAbilities);
-    abilities.academics = 1;
-    abilities.alertness = 2;
-    abilities.animalken = 3;
-    const abilitiesExtension = R.clone(initialAbilitiesExtension);
-    expect(
-      checkAbilitiesDotLimit(
-        abilities,
-        vampireAbilitiesConfig,
-        abilitiesExtension,
-        ABILITY_LIMIT
-      )
-    ).toStrictEqual(true);
-  });
-  it("Main ability is greater than ABILITY_LIMIT - invalid", () => {
-    const abilities = R.clone(initialAbilities);
-    abilities.academics = 4;
-    const abilitiesExtension = R.clone(initialAbilitiesExtension);
-    expect(
-      checkAbilitiesDotLimit(
-        abilities,
-        vampireAbilitiesConfig,
-        abilitiesExtension,
-        ABILITY_LIMIT
-      )
-    ).toStrictEqual(false);
-  });
+    it("Main abilities are less or equal to ABILITY_LIMIT - valid", () => {
+      const abilities = R.clone(initialAbilities);
+      abilities.academics = 1;
+      abilities.alertness = 2;
+      abilities.animalken = 3;
+      const abilitiesExtension = R.clone(initialAbilitiesExtension);
+      expect(
+        checkAbilitiesDotLimit(
+          abilities,
+          vampireAbilitiesConfig,
+          abilitiesExtension,
+          ABILITY_LIMIT
+        )
+      ).toStrictEqual(true);
+    });
+    it("Main ability is greater than ABILITY_LIMIT - invalid", () => {
+      const abilities = R.clone(initialAbilities);
+      abilities.academics = 4;
+      const abilitiesExtension = R.clone(initialAbilitiesExtension);
+      expect(
+        checkAbilitiesDotLimit(
+          abilities,
+          vampireAbilitiesConfig,
+          abilitiesExtension,
+          ABILITY_LIMIT
+        )
+      ).toStrictEqual(false);
+    });
 
-  it("Extra abilities are less or equal to ABILITY_LIMIT - valid", () => {
-    const abilities = R.clone(initialAbilities);
-    const abilitiesExtension = R.clone(initialAbilitiesExtension);
-    abilitiesExtension.knowledgeValue1 = 1;
-    abilitiesExtension.knowledgeValue2 = 2;
-    abilitiesExtension.skillValue1 = 3;
-    expect(
-      checkAbilitiesDotLimit(
-        abilities,
-        vampireAbilitiesConfig,
-        abilitiesExtension,
-        ABILITY_LIMIT
-      )
-    ).toStrictEqual(true);
-  });
-  it("Extra ability is bigger than ABILITY_LIMIT - invalid", () => {
-    const abilities = R.clone(initialAbilities);
-    const abilitiesExtension = R.clone(initialAbilitiesExtension);
-    abilitiesExtension.knowledgeValue1 = 4;
-    expect(
-      checkAbilitiesDotLimit(
-        abilities,
-        vampireAbilitiesConfig,
-        abilitiesExtension,
-        ABILITY_LIMIT
-      )
-    ).toStrictEqual(false);
+    it("Extra abilities are less or equal to ABILITY_LIMIT - valid", () => {
+      const abilities = R.clone(initialAbilities);
+      const abilitiesExtension = R.clone(initialAbilitiesExtension);
+      abilitiesExtension.knowledgeValue1 = 1;
+      abilitiesExtension.knowledgeValue2 = 2;
+      abilitiesExtension.skillValue1 = 3;
+      expect(
+        checkAbilitiesDotLimit(
+          abilities,
+          vampireAbilitiesConfig,
+          abilitiesExtension,
+          ABILITY_LIMIT
+        )
+      ).toStrictEqual(true);
+    });
+    it("Extra ability is bigger than ABILITY_LIMIT - invalid", () => {
+      const abilities = R.clone(initialAbilities);
+      const abilitiesExtension = R.clone(initialAbilitiesExtension);
+      abilitiesExtension.knowledgeValue1 = 4;
+      expect(
+        checkAbilitiesDotLimit(
+          abilities,
+          vampireAbilitiesConfig,
+          abilitiesExtension,
+          ABILITY_LIMIT
+        )
+      ).toStrictEqual(false);
+    });
   });
 });
