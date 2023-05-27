065d06e2cfe04549bb08aee871659ff013c274fa
NtsDK <ghoul71@yandex.ru>
2023-05-28 03:25:06 +0500

    Work on vtda dropdowns

---
+++ src/charSheets/vtm/dropdownContent/resources/conceptsSource copy.ts
@@ -0,0 +1,50 @@
+// Format:
+// <en>
+//   <ru>
+
+export const conceptsSource = `Entertainer
+  Артист
+Soldier
+  Боец
+Drifter
+  Бродяга
+Outsider
+  Изгой
+Intellectual
+  Интеллектуал
+Scenester
+  Неформал
+Kid
+  Подросток
+Politician
+  Политик
+Criminal
+  Преступник
+Professional
+  Профессионал
+Worker
+  Работяга
+Reporter
+  Репортёр
+Socialite
+  Светский лев
+Investigator
+  Сыщик`;
+
+// +
+
+// Cleric
+//   Духовное лицо
+
+//   Child
+//   Ребёнок
+
+// -
+
+// Неформал
+
+// Подросток
+
+// Репортёр
+
+// Светский лев
