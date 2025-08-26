2f3d33acc61acaa4df6dd6a914dd13c76cbcc468
Timofey Rechkalov <trechkalov@yandex.ru>
2025-08-27 03:12:34 +0500

    Initial MtA charsheet component

--- src/charSheets/mta/CharSheet.tsx
+++ src/charSheets/mta/CharSheet.tsx
@@ -1,5 +1,120 @@
-// Mage the Ascension Character Sheet main component
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-export function CharSheet() {
-  return <div>Mage: The Ascension Character Sheet (WIP)</div>;
+import {
+  useAlliesAndContacts,
+  useAppearance,
+  useCharHistory,
+  useNotes,
+  useOtherTraits,
+  usePossessions,
+} from "../generic/services/storageAdapter";
+import {
+  CharSheetBody,
+  SectionHeader,
+  Subheader,
+} from "../generic/uiPrimitives";
+import {
+  OtherTraitsSection,
+  CharHistorySection,
+  GoalsSection,
+  AlliesAndContactsSection,
+  PossessionsSection,
+  CharacterImageSection,
+  AppearanceDescriptionSection,
+  NotesSection,
+  CharSheetStarter,
+} from "../generic/uiSections";
+import { useLimits } from "../root/services/storageAdapter";
+
+// import { AdvantagesSection } from "./uiSections/AdvantagesSection";
+// import { StatusSection } from "./uiSections/StatusSection";
+
+interface CharSheetProps {}
+
+export function CharSheet(props: CharSheetProps): JSX.Element {
+  const { t } = useTranslation();
+
+  const { limits } = useLimits();
+
+  const otherTraitsService = useOtherTraits();
+  const historyService = useCharHistory();
+  const alliesAndContactsService = useAlliesAndContacts();
+  const possessionsService = usePossessions();
+  const appearanceService = useAppearance();
+  const notesService = useNotes();
+
+  return (
+    <>
+      <CharSheetBody>
+        <CharSheetStarter />
+        <SectionHeader className="tw-mb-3">
+          {t("charsheet.advantages.header")}
+        </SectionHeader>
+        {/* <AdvantagesSection className="tw-mb-3" /> */}
+      </CharSheetBody>
+      {/* <CharSheetBody>
+        <SectionHeader className="tw-mb-3">
+          {t("charsheet.status.header")}
+        </SectionHeader>
+        <StatusSection className="tw-mb-3" />
+
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t("charsheet.advantages.otherTraits")}
+            </SectionHeader>
+            <OtherTraitsSection limits={limits} {...otherTraitsService} />
+          </div>
+
+          <div className="tw-flex-1" style={{ flexGrow: 2 }}>
+            <SectionHeader className="tw-mb-3">
+              {t("charsheet.charHistory")}
+            </SectionHeader>
+            <CharHistorySection className="tw-mb-6" {...historyService} />
+
+            <SectionHeader className="tw-mb-3">
+              {t("charsheet.goals")}
+            </SectionHeader>
+            <GoalsSection className="tw-mb-6" {...historyService} />
+          </div>
+        </div>
+
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t("charsheet.alliesAndContacts")}
+            </SectionHeader>
+            <AlliesAndContactsSection {...alliesAndContactsService} />
+          </div>
+
+          <div className="tw-flex-1">
+            <SectionHeader className="tw-mb-3">
+              {t("charsheet.possessions")}
+            </SectionHeader>
+            <PossessionsSection {...possessionsService} />
+          </div>
+        </div>
+      </CharSheetBody>
+      <CharSheetBody>
+        <SectionHeader className="tw-mb-3">
+          {t("charsheet.appearanceDescription")}
+        </SectionHeader>
+        <div className="tw-flex tw-gap-x-4 tw-mb-6">
+          <div className="tw-flex-1">
+            <CharacterImageSection {...appearanceService} />
+          </div>
+
+          <div className="tw-flex-1" style={{ flexGrow: 2 }}>
+            <AppearanceDescriptionSection {...appearanceService} />
+          </div>
+        </div>
+
+        <SectionHeader className="tw-mb-3">
+          {t("charsheet.notes")}
+        </SectionHeader>
+        <NotesSection {...notesService} />
+      </CharSheetBody> */}
+    </>
+  );
 }
